41e04ad8be6f5bddb639799a391af67dbc5999c3
Haruka31Dev <u202412824@upc.edu.pe>
2025-05-06 00:03:04 -0500

    fix

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -22,7 +22,7 @@ class CorsMiddleware
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With,enctype'
+            'Access-Control-Allow-Headers'     => 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, enctype'
         ];
 
         if ($request->isMethod('OPTIONS'))
